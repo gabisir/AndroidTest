33589034d59c93c6711317b53c4d321934851402
gabisir <105482725+gabisir@users.noreply.github.com>
2024-02-01 11:28:11 +0100

    Initial commit

---
+++ app/src/main/java/com/example/androidtest/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.androidtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(applicationContext, "onStart", Toast.LENGTH_SHORT).show()
+        Log.i("MyLog", "onStart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(applicationContext, "onPause", Toast.LENGTH_SHORT).show()
+        Log.i("MyLog", "onPause")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(applicationContext, "onResume", Toast.LENGTH_SHORT).show()
+        Log.i("MyLog", "onResume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(applicationContext, "onRestart", Toast.LENGTH_SHORT).show()
+        Log.i("MyLog", "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(applicationContext, "onDestroy", Toast.LENGTH_SHORT).show()
+        Log.i("MyLog", "onDestroy")
+
+    }
+    var counter=0
+    fun onButtonUpClicked(view: View) {
+        counter++
+        val text1: TextView = findViewById(R.id.Counter)
+        text1.text = counter.toString()
+    }
+    fun onButtonDownClicked(view: View) {
+        if(counter!=0)
+            counter--
+        val text1: TextView = findViewById(R.id.Counter)
+        text1.text = counter.toString()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putInt("counter", counter)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        counter = savedInstanceState.getInt("counter", 0)
+        findViewById<TextView>(R.id.Counter).text = counter.toString()
+    }
+
+}
