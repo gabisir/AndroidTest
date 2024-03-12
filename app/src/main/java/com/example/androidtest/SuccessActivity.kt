b7c556e96cef55782c06451d27a1a010d24f2ba9
gabisir <gabrijel802@gmail.com>
2024-03-12 22:48:02 +0100

    Added function to radio group

--- app/src/main/java/com/example/androidtest/SuccessActivity.kt
+++ app/src/main/java/com/example/androidtest/SuccessActivity.kt
@@ -1,5 +1,6 @@
 package com.example.androidtest
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -11,6 +12,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class SuccessActivity : AppCompatActivity() {
+    var phoneNumberChosen : String ="0919445464"
     lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,20 +23,13 @@ class SuccessActivity : AppCompatActivity() {
         textView.text="$name, $successText"
     }
 
+    @SuppressLint("QueryPermissionsNeeded")
     fun successButtonClicked(view: View) {
         val message = textView.text.toString()
-        var phoneNumber=findViewById<TextView>(R.id.editTextPhone).text.toString()
-        val defaultPhoneNumber="0919445464"
-        if(phoneNumber=="")
-            phoneNumber=defaultPhoneNumber
-
-        val radioGroup = findViewById<RadioGroup>(R.id.Phone1)
-        val odabir = radioGroup.checkedRadioButtonId
-        val selectedButton=findViewById<RadioGroup>(odabir)
-        val selectedText = selectedButton.text.toString()
-
-
-            val uri = Uri.parse("smsto:$phoneNumber")
+        val phoneNumber=findViewById<TextView>(R.id.editTextPhone).text.toString()
+        if(phoneNumber!="")
+            phoneNumberChosen = phoneNumber
+        val uri = Uri.parse("smsto:$phoneNumberChosen")
         val smsIntent = Intent(Intent.ACTION_SENDTO, uri)
         smsIntent.putExtra("sms_body", message)
         val chooser: Intent = Intent.createChooser(smsIntent, "Share")
@@ -44,5 +39,28 @@ class SuccessActivity : AppCompatActivity() {
             Toast.makeText(this, "No SMS app found", Toast.LENGTH_SHORT).show()
     }
 
-    fun successbuttonclicked(view: View) {}
+    fun radioButton1(view: View) {
+        val radioGroup: RadioGroup = findViewById(R.id.RadioGroup)
+        val choice = radioGroup.checkedRadioButtonId
+        val chosenButton : RadioButton = findViewById(choice)
+        //Toast.makeText(this, choice.toString(), Toast.LENGTH_SHORT).show()
+        phoneNumberChosen = chosenButton.text.toString()
+        Toast.makeText(this, phoneNumberChosen, Toast.LENGTH_SHORT).show()
+    }
+    fun radioButton2(view: View) {
+        val radioGroup: RadioGroup = findViewById(R.id.RadioGroup)
+        val choice = radioGroup.checkedRadioButtonId
+        val chosenButton : RadioButton = findViewById(choice)
+        //Toast.makeText(this, choice.toString(), Toast.LENGTH_SHORT).show()
+        phoneNumberChosen = chosenButton.text.toString()
+        Toast.makeText(this, phoneNumberChosen, Toast.LENGTH_SHORT).show()
+    }
+    fun radioButton3(view: View) {
+        val radioGroup: RadioGroup = findViewById(R.id.RadioGroup)
+        val choice = radioGroup.checkedRadioButtonId
+        val chosenButton : RadioButton = findViewById(choice)
+        //Toast.makeText(this, choice.toString(), Toast.LENGTH_SHORT).show()
+        phoneNumberChosen = chosenButton.text.toString()
+        Toast.makeText(this, phoneNumberChosen, Toast.LENGTH_SHORT).show()
+    }
 }
